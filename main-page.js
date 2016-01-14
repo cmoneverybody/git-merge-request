cdbd6d53a69c059657cd65a5cdfac57bc0cf8d13
cmoneverybody <as.avramenko@yandex.ru>
2016-01-14 17:02:22 +0300

    Update main-page.js

--- main-page.js
+++ main-page.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
       replaced;
    if (container.length) {
       url = container.find('.btn').attr('href');
-      replaced = url.search('sbis3-controls') !== -1 ? 'development' : 'rc-3.7.3.20';
+      replaced = url.search('sbis3-controls') !== -1 ? 'development' : 'rc-3.7.3.15';
       container
          .prepend('<a class="ws-button" target="_blank" href=' + url.replace(replaced, 'development') + '>dev</a>')
          .prepend('<a class="ws-button" target="_blank" href=' + url.replace(replaced, 'rc-3.7.4') + '>3.7.4</a>')
