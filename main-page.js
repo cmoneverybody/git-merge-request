7a8091e835795613cb44174a498023c4720cfe7d
as.avramenko <as.avramenko@tensor.ru>
2016-05-06 19:03:44 +0300

    Тесты на Дженкинс

--- main-page.js
+++ main-page.js
@@ -5,11 +5,11 @@ $(document).ready(function() {
       replaced;
    if (container.length) {
       url = container.find('.btn').attr('href');
-      replaced = url.search('sbis3-controls') !== -1 ? 'development' : 'rc-3.7.3.15';
+      replaced = /*url.search('sbis3-controls') !== -1 ? 'development' : */'rc-3.7.3.150';
       container
          .prepend('<a class="ws-button" target="_blank" href=' + url.replace(replaced, 'development') + '>dev</a>')
          .prepend('<a class="ws-button" target="_blank" href=' + url.replace(replaced, 'rc-3.7.4') + '>3.7.4</a>')
-         .prepend('<a class="ws-button" target="_blank" href=' + url.replace(replaced, 'rc-3.7.3.140') + '>3.7.3.140</a>')
-         .prepend('<a class="ws-button" target="_blank" href=' + url.replace(replaced, 'rc-3.7.3.100') + '>3.7.3.100</a>');
+         .prepend('<a class="ws-button" target="_blank" href=' + url.replace(replaced, 'rc-3.7.3.200') + '>3.7.3.200</a>')
+         .prepend('<a class="ws-button" target="_blank" href=' + url.replace(replaced, 'rc-3.7.3.150') + '>3.7.3.150</a>');
    }
 });
--- merge-request.js
+++ merge-request.js
@@ -6,7 +6,15 @@ $(document).ready(function() {
       plus1Target = $('.voting_notes'),
       plus1 = $('<div class="ws-button plus1">+1 без тестов</div>'),
       mrText = $('.js-main-target-form .note_text'),
-      mrForm = mrText.parents('form');
+      mrForm = mrText.parents('form'),
+      jenkinsConst = 'http://test-jenkins/job/sbis3-controls_',
+      jenkinsMiddle = '/branch/'
+      jenkinsIconConst = '/badge/icon',
+      branch = $('.mr-source-target .label-branch:first').html(),
+      rootBranch = branch.substring(0, branch.indexOf('/')),
+      testURL = jenkinsConst + rootBranch + jenkinsMiddle + encodeURIComponent(branch),
+      testIconURL = testURL + jenkinsIconConst;
+      
    $('body').addClass(mrMerged ? 'ws-mr-already-merged' : mrError ? 'ws-mr-not-merged' : 'ws-mr-merged');
    if (plus1Target.length) {
       plus1.click(function() {
@@ -15,4 +23,6 @@ $(document).ready(function() {
       });
       plus1.prependTo(plus1Target);
    }
+      
+   $('<div class="request-tests-block" style="margin: 16px 0;"><img src="http://test-jenkins/static/5661bc22/images/headshot.png" style="height: 24px; vertical-align: top; padding-right: 8px;"><a href="' + testURL + '" style="color: #3084bb; font-weight: bold; display: inline-block; font-size: 16px; padding-right: 8px;">Jenkins tests</a><img src="' + testIconURL + '" style="display: inline-block; vertical-align: top;"></div>').insertAfter($('.merge-request .middle-block'));
 });
