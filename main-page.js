eeea2d7ba7ba69fe1fc56c9798b36bf1a5b5e9a2
as.avramenko <as.avramenko@tensor.ru>
2016-05-13 11:47:08 +0300

    3.7.4.100

--- main-page.js
+++ main-page.js
@@ -8,6 +8,7 @@ $(document).ready(function() {
       replaced = /*url.search('sbis3-controls') !== -1 ? 'development' : */'rc-3.7.3.150';
       container
          .prepend('<a class="ws-button" target="_blank" href=' + url.replace(replaced, 'development') + '>dev</a>')
+         .prepend('<a class="ws-button" target="_blank" href=' + url.replace(replaced, 'rc-3.7.4.100') + '>3.7.4.100</a>')
          .prepend('<a class="ws-button" target="_blank" href=' + url.replace(replaced, 'rc-3.7.4') + '>3.7.4</a>')
          .prepend('<a class="ws-button" target="_blank" href=' + url.replace(replaced, 'rc-3.7.3.200') + '>3.7.3.200</a>')
          .prepend('<a class="ws-button" target="_blank" href=' + url.replace(replaced, 'rc-3.7.3.150') + '>3.7.3.150</a>');
