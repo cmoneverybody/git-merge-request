532180b85e40dbe664ec7f6cf68df5e1b2468ad9
as.avramenko <as.avramenko@tensor.ru>
2016-07-05 14:52:57 +0300

    150->200

--- main-page.js
+++ main-page.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
       replaced;
    if (container.length) {
       url = container.find('.btn').attr('href');
-      replaced = /*url.search('sbis3-controls') !== -1 ? 'development' : */'rc-3.7.3.150';
+      replaced = /*url.search('sbis3-controls') !== -1 ? 'development' : */'rc-3.7.3.200';
       container
          .prepend('<a class="ws-button" target="_blank" href=' + url.replace(replaced, 'development') + '>dev</a>')
          .prepend('<a class="ws-button" target="_blank" href=' + url.replace(replaced, 'rc-3.7.4.100') + '>3.7.4.100</a>')
