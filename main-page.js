5072df4eeecf4073b63302d7d412c3c3f3a7c4b5
as.avramenko <as.avramenko@tensor.ru>
2016-07-05 15:46:40 +0300

    универсальный скрипт

--- main-page.js
+++ main-page.js
@@ -1,16 +1,17 @@
 $(document).ready(function() {
    var
+      branches = ['3.7.4.100', '3.7.4', '3.7.3.230', '3.7.3.220'],
       container = $('.event-last-push .pull-right'),
       url,
       replaced;
    if (container.length) {
       url = container.find('.btn').attr('href');
-      replaced = /*url.search('sbis3-controls') !== -1 ? 'development' : */'rc-3.7.3.200';
-      container
-         .prepend('<a class="ws-button" target="_blank" href=' + url.replace(replaced, 'development') + '>dev</a>')
-         .prepend('<a class="ws-button" target="_blank" href=' + url.replace(replaced, 'rc-3.7.4.100') + '>3.7.4.100</a>')
-         .prepend('<a class="ws-button" target="_blank" href=' + url.replace(replaced, 'rc-3.7.4') + '>3.7.4</a>')
-		 .prepend('<a class="ws-button" target="_blank" href=' + url.replace(replaced, 'rc-3.7.3.220') + '>3.7.3.230</a>')
-		 .prepend('<a class="ws-button" target="_blank" href=' + url.replace(replaced, 'rc-3.7.3.220') + '>3.7.3.220</a>');
+      url.search(/(?:target_branch%5D=)([^&]+)/gim, function(res, branch) {
+          replaced = branch;
+      });
+      container.prepend('<a class="ws-button" target="_blank" href=' + url.replace(replaced, 'development') + '>dev</a>');
+      for (var i = 0; i < branches.length; i++) {
+         container.prepend('<a class="ws-button" target="_blank" href=' + url.replace(replaced, 'rc-' + branches[i]) + '>' + branches[i] + '</a>')
+      }
    }
 });
