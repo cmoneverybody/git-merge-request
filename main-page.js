47e752d679bf26984a1d9b4f7fc104e898f6f4e3
as.avramenko <as.avramenko@tensor.ru>
2016-07-14 17:59:25 +0300

    create another request

--- main-page.js
+++ main-page.js
@@ -3,7 +3,55 @@ $(document).ready(function() {
       branches = ['3.7.4.200', '3.7.4.100', '3.7.4', '3.7.3.230'],
       container = $('.event-last-push .pull-right'),
       url,
-      replaced;
+      replaced,
+      createRequest = $('<div class="ws-create-request" title="Create another request"><i class="ws-create-request-button fa fa-pencil-square-o"</i></div>'),
+      rp1 = 'https://git.sbis.ru/sbis/controls/merge_requests/new?utf8=✓&merge_request[source_project_id]=143&merge_request[source_branch]=',
+      rp2 = '&merge_request[target_project_id]=143&merge_request[target_branch]=';
+      createRequest
+      .prependTo($('.input-group'))
+      .find('i').click(function() {
+         var
+            pos = createRequest.find('i').offset(),
+            requestContainer,
+            branchName;
+         if (window.createRequestFrame) {
+            window.createRequestFrame.remove();
+         }
+         window.createRequestFrame = $('<div class="ws-create-request-frame" style="display: none;"><input placeholder="You must select source and target branch" class="ws-branch-name"><div class="ws-create-request-links"></div></div>')
+            .appendTo('body')
+            .css(pos);
+         requestContainer = $('.ws-create-request-links');
+         requestContainer.prepend('<a class="ws-button" branch="development">dev</a>');
+         for (var i = 0; i < branches.length; i++) {
+            requestContainer.prepend('<a class="ws-button" branch="rc-' + branches[i] + '">' + branches[i] + '</a>');
+         }
+         window.createRequestFrame.fadeIn(200, function() {
+            $('.ws-branch-name').focus()
+         });
+      });
+   document.addEventListener('click', function(e) {
+      var
+         targetBranch = e.target.getAttribute('branch'),
+         branchName, branchField;
+      if (targetBranch) {
+         branchField = $('.ws-branch-name');
+         branchName = branchField.val();
+         if (branchName) {
+            window.open(rp1 + branchName + rp2 + targetBranch,'_blank');
+         } else {
+            branchField.focus();
+         }
+      } else if (window.createRequestFrame && !$(e.target).closest(window.createRequestFrame).length) {
+         window.createRequestFrame.remove();
+         window.createRequestFrame = undefined;
+      }
+   }, true);
+   document.addEventListener('keydown', function(e) {
+      if (e.keyCode === 27 && e.target.className === 'ws-branch-name' && window.createRequestFrame) {
+         window.createRequestFrame.remove();
+         window.createRequestFrame = undefined;
+      };
+   }, true);
    if (container.length) {
       url = container.find('.btn').attr('href');
       url.replace(/(?:target_branch%5D=)([^&]+)/gim, function(res, branch) {
