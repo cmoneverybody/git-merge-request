c05dea829ff263290fbf2fe1f32bcea3441e64e9
as.avramenko <as.avramenko@tensor.ru>
2016-09-01 11:47:00 +0300

    - 3.7.4.100 + 3.7.4.130

--- main-page.js
+++ main-page.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
    var
-      branches = ['3.7.4.200', '3.7.4.120', '3.7.4.110', '3.7.4.100'],
+      branches = ['3.7.4.200', '3.7.4.130', '3.7.4.120', '3.7.4.110'],
       container = $('.event-last-push .pull-right'),
       url,
       replaced,
