c46b6ec68c220ebc6bdc4400f5ad4d119bad3a8c
as.avramenko <as.avramenko@tensor.ru>
2017-01-09 11:57:08 +0300

    fix after git updates

--- merge-request.js
+++ merge-request.js
@@ -3,23 +3,22 @@ $(document).ready(function() {
       mrState = $('.mr-state-widget'),
       mrError = !!mrState.find('.fa-exclamation-triangle').length,
       mrMerged = !!mrState.find('.author_link').length,
-      plus1Target = $('.voting_notes'),
-      plus1 = $('<div class="ws-button plus1" style="width: 70px;">+ 1</div>'),
-      mrText = $('.js-main-target-form .note_text'),
+      plus1Target = $('.voting_notes .content-block .awards'),
+      plus1 = $('<div class="award-control btn plus1">+ 1</div>'),
+      mrText = $('.js-main-target-form .js-note-text'),
       mrForm = mrText.parents('form'),
       jenkinsConst = 'http://test-jenkins/job/' + (document.location.href.indexOf('/ws/data/') !== -1 ? 'ws.data' : document.location.href.indexOf('/sbis/ws') !== -1 ? 'ws' : 'sbis3-controls')  + '_',
-      jenkinsMiddle = '/branch/'
+      jenkinsMiddle = '/branch/',
       jenkinsIconConst = '/badge/icon',
-      branch = $('.mr-source-target .label-branch:first').html(),
+      branch = $('.mr-source-target .label-branch:first a').html(),
       rootBranch = branch.substring(0, branch.indexOf('/')),
       testURL = jenkinsConst + rootBranch + jenkinsMiddle + encodeURIComponent(branch),
       testIconURL = testURL + jenkinsIconConst;
-      
    $('<div class="ws-copy-link fa fa-files-o" title="Copy merge request URL"></div>')
       .click(function() {
          document.execCommand('copy');
       })
-      .prependTo('body');
+      .insertBefore('.header-logo .home');
       
    this.addEventListener('copy', function (e) {
       if (e.target.className.indexOf('ws-copy-link') !== -1) {
@@ -32,9 +31,9 @@ $(document).ready(function() {
    if (plus1Target.length) {
       plus1.click(function() {
          mrText.html('+1');
-         mrText.parents('form').submit();
+         mrForm.submit();
       });
       plus1.prependTo(plus1Target);
    }
-   $('<div class="request-tests-block" style="margin: 16px 0;"><img src="http://test-jenkins/static/5661bc22/images/headshot.png" style="height: 24px; vertical-align: top; padding-right: 8px;"><a href="' + testURL + '" style="color: #3084bb; font-weight: bold; display: inline-block; font-size: 16px; padding-right: 8px;">Jenkins tests</a><img src="' + testIconURL + '" style="display: inline-block; vertical-align: top;"></div>').insertAfter($('.merge-request .middle-block'));
+   $('<div class="request-tests-block" style="margin: 16px 0;"><img src="http://test-jenkins/static/5661bc22/images/headshot.png" style="height: 24px; vertical-align: top; padding-right: 8px;"><a href="' + testURL + '" style="color: #3084bb; font-weight: bold; display: inline-block; font-size: 16px; padding-right: 8px;">Jenkins tests</a><img src="' + testIconURL + '" style="display: inline-block; vertical-align: top;"></div>').insertBefore($('.merge-request .mr-state-widget'));
 });
